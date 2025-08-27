7199a9432cb80141fedb9c9145a20d3893eac9ff
Hilal789 <smhilal789@gmail.com>
2025-08-28 00:34:46 +0530

    Add new project

---
+++ YouTubePractice/src/Component/CheckTaiwind/tailwind.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Card from './Card'
+
+function Tailwind() {
+
+    let name = {
+        username : "Bal",
+        age : 22,
+        chanel : "BalBoka"
+    }
+  return (
+    <div>
+        <Card username="hilal"/>
+        <Card username="sm"/>
+        <Card/>
+
+    </div>
+  )
+}
+
+export default Tailwind
+
+
--- YouTubePractice/src/main.jsx
+++ YouTubePractice/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Tailwind from './Component/CheckTaiwind/tailwind.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <Tailwind />
   </StrictMode>,
 )
